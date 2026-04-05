37ed685a1dd8e86deeee01e5dab8600f922407d0
H. S. Nam Bình <nahmsix2005@gmail.com>
2026-04-06 00:34:30 +0700

    add asmx web services

---
+++ BT3_WebApplication/BT3_WebApplication/WebService1.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WebService1.asmx.cs" Class="BT3_WebApplication.WebService1" %>
---
+++ BT3_WebApplication/BT3_WebApplication/WebService1.asmx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace BT3_WebApplication
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld(string name)
+        {
+            return "Hello, " + name;
+        }
+
+        [WebMethod]
+        public int AddCalculator(int a, int b)
+        {
+            return a + b;
+        }
+
+        [WebMethod]
+        public int SubtractCalculator(int a, int b)
+        {
+            return a - b;
+        }
+
+        [WebMethod]
+        public int MultiplyCalculator(int a, int b)
+        {
+            return a * b;
+        }
+
+        [WebMethod]
+        public double DivideCalculator(int a, int b)
+        {
+
+            if (b == 0)
+            {
+                throw new Exception("cannot divide by zero");
+            }
+            else
+            {
+                return (double)a / b;
+            }
+        }
+    }
+}
